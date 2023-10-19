4114a6b725bace62e56caa91f41cbc7a0dca5b3b
Jiwon <zzwon1212@gmail.com>
2023-10-20 00:18:43 +0900

    chore: Build

---
+++ build/lidar_drive/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jiwon/xycar_ws/src/lidar_drive
+# Build directory: /home/jiwon/xycar_ws/build/lidar_drive
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
